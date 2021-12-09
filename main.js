a9a45b00b4e217dd26d5de50ca0389c365b0ee3b
sspboyd <stephen@sspboyd.ca>
2021-12-08 23:21:45 -0500

    Working version with keyboard controls and copy added to page.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import "./style.css";
 import P5 from "p5";
-import yyz from "./media/yyz.jpeg";
-import xmas from "./media/xmas.jpeg";
-import xmas2 from "./media/wonderful_life.jpg";
-import xmas3 from "./media/rudolph.jpeg";
-import xmas4 from "./media/xmas.jpeg";
+// import yyz from "./media/yyz.jpeg";
+// import white_xmas from "./media/xmas.jpeg";
+import wonderful_life from "./media/wonderful_life.jpg";
+import rudolph from "./media/rudolph.jpeg";
+import elf from "./media/elf.jpg";
+// import p5 from "p5";
 // import p5 from "p5";
 
 /*
@@ -24,10 +25,17 @@ create these en masse by randomly setting node points and saving output (probabl
 
 */
 
-let base_img, src_img;
+let src_img;
+let src_imgs = [];
+let src_img_names = [wonderful_life, rudolph, elf];
+console.log({ src_img_names });
 let jb_counter = 0;
+let src_img_idx = 1;
+let canvas_state = "intro";
+const fib_47 = [3, 4, 7, 11, 18];
 
 const create_jawbreaker = function (p55, ox, oy) {
+  let ring_sz_idx = Math.floor(Math.random() * fib_47.length);
   return {
     active: true,
     jb_id: jb_counter++,
@@ -35,9 +43,11 @@ const create_jawbreaker = function (p55, ox, oy) {
     // edge: { x: -1, y: -1 },
     orig_vec: p55.createVector(ox, oy),
     edge_vec: p55.createVector(),
-    ring_sz: 4,
-    ring_num: 7 + Math.random() * 76,
+    ring_sz: fib_47[ring_sz_idx],
+    // ring_sz: 7,
+    ring_num: 11 + Math.random() * 47,
     ring_radius_scl: 1,
+    ring_alpha: 0.29,
     // radius: 1,
     rings: [], // contains ring {}s with orig, edge, clr, size
     activeStatus: function () {
@@ -74,29 +84,30 @@ const create_jawbreaker = function (p55, ox, oy) {
           };
         }
       }
-      // this.radius = 100;
-      // this.radius = this.orig_vec.dist(this.edge_vec) * this.radius_scl;
+    },
+    render: function () {
+      this.rings.forEach((r) => {
+        // get color
+        p55.stroke(r.ring_clr, r.ring_alpha);
+        p55.strokeWeight(r.ring_sz);
+        p55.noFill();
+        p55.ellipse(this.orig_vec.x, this.orig_vec.y, r.ring_radius * 2);
+      });
       p55.strokeWeight(0.76);
-      p55.stroke(0, 123);
+      p55.stroke(0, 199);
       p55.line(
         this.orig_vec.x,
         this.orig_vec.y,
         this.edge_vec.x,
         this.edge_vec.y
       );
-    },
-    render: function () {
-      // render rings
-      // for each ring (this.ring_num)
-      // find that spot on the line (lerp?) and get the color.
-      // Then draw a circle.
-      this.rings.forEach((r) => {
-        // get color
-        p55.stroke(r.ring_clr);
-        p55.strokeWeight(r.ring_sz);
-        p55.noFill();
-        p55.ellipse(this.orig_vec.x, this.orig_vec.y, r.ring_radius * 2);
-      });
+      // let pointer_clr = src_img.get(p55.mouseX, p55.mouseY);
+      // if (this.rings[this.rings.length - 1].ring_clr !== undefined)
+      //   p55.fill(this.rings[this.rings.length - 1].ring_clr);
+      // Draw the pointer
+      p55.fill(src_img.get(p55.mouseX, p55.mouseY));
+      p55.noStroke();
+      p55.ellipse(p55.mouseX, p55.mouseY, 47, 47);
     },
   };
 
@@ -173,6 +184,7 @@ const s = (p55) => {
   let c; // createCanvas variable
   let canvasW = 1080;
   let canvasH = 1080;
+  let img_show = false;
   // let copy_font;
 
   let jawbreakers = []; // var to hold all the jb objects
@@ -181,21 +193,36 @@ const s = (p55) => {
     // preload one of the images in here.
     // copy_font = p55.loadFont('assets/fonts/Georgia.ttf');
     // src_img =  p55.loadImage(yyz);
-    src_img = p55.loadImage(xmas3);
+    for (let i = 0; i < src_img_names.length; i++) {
+      src_imgs[i] = p55.loadImage(src_img_names[i]);
+    }
+    src_img = src_imgs[src_img_idx];
   };
 
   p55.setup = () => {
+    if (p55.pixelDensity() > 2) p55.pixelDensity(2);
     canvasW = src_img.width;
     canvasH = src_img.height;
     c = p55.createCanvas(canvasW, canvasH);
+    // p55.image(src_img, 0, 0);
     // c.parent(sketch_name); // used to associate this sketch with an html div id
   };
 
   p55.draw = () => {
-    // only update if the mouse has moved
-    if (p55.mouseX != p55.pmouseX || p55.mouseY != p55.pmouseY) {
+    if (canvas_state === "intro") {
       p55.background(255);
       p55.image(src_img, 0, 0);
+      p55.fill(0, 123);
+      p55.rect(0, 0, canvasW, canvasH);
+      p55.fill(255);
+      p55.textSize(72);
+      p55.textFont("Helvetica");
+      p55.text("Click Around and \nFind Out...", 199, 321);
+    } else {
+      // only update if the mouse has moved
+      // if (p55.mouseX != p55.pmouseX || p55.mouseY != p55.pmouseY) {
+      p55.background(255);
+      if (p55.keyIsDown(p55.SHIFT)) p55.image(src_img, 0, 0);
       jawbreakers.forEach((jbkr) => {
         // console.log(
         //   `In draw() bf render() called: jbkr.jb_id = ${jbkr.jb_id}. jbkr.active = ${jbkr.active}`
@@ -204,10 +231,13 @@ const s = (p55) => {
         jbkr.update();
         jbkr.render();
       });
+      watermark();
+      // }
     }
   };
 
   p55.mousePressed = () => {
+    if (canvas_state === "intro") canvas_state = "playable";
     // tests to see if this is the first jb obj created...
     if (jawbreakers.length > 0) {
       // if more than 0 jbs in the array then
@@ -217,6 +247,7 @@ const s = (p55) => {
       curr_jb.edge_vec.y = p55.mouseY;
       // set active = false
       curr_jb.set_activeStatus(false);
+      curr_jb.ring_alpha = 1;
       // console.log(
       //   `jawbreakers[jawbreakers.length - 1].active = ${
       //     jawbreakers[jawbreakers.length - 1].active
@@ -234,6 +265,51 @@ const s = (p55) => {
     //   //   `In mousePressed: jbkr.jb_id = ${jbkr.jb_id}. jbkr.active = ${jbkr.active}`
     //   // );
     // });
+  };
+
+  p55.keyPressed = function () {
+    if (p55.key === "i") {
+      if (src_img_idx === src_img_names.length - 1) {
+        src_img_idx = 0;
+      } else {
+        src_img_idx++;
+      }
+      src_img = src_imgs[src_img_idx];
+      jawbreakers = [];
+      p55.image(src_img, 0, 0);
+      canvas_state = "intro";
+      // console.log(`Inside key==='i'`);
+      // console.log({ src_img_idx, src_img });
+    }
+    if (p55.key === "r") {
+      jawbreakers = [];
+      canvas_state = "intro";
+      p55.image(src_img, 0, 0);
+    }
+    if (p55.key === "s" || p55.key === "d") {
+      export_canvas();
+    }
+  };
+
+  const export_canvas = function () {
+    const file_name = "sspboyd-Christmas-Card";
+    const dateObj = new Date();
+    const year = dateObj.getFullYear();
+    const month = dateObj.getMonth() + 1; //months from 1-12
+    const day = dateObj.getDate();
+    const hrs = dateObj.getHours();
+    const mins = dateObj.getMinutes();
+    const secs = dateObj.getSeconds();
+    const file_id = `${year}${month}${day}${hrs}${mins}${secs}`;
+
+    p55.saveCanvas(c, `${file_name}_${file_id}`, "jpg");
+  };
+
+  const watermark = function () {
+    p55.textSize(12);
+    p55.fill(255, 199);
+    p55.noStroke();
+    p55.text("@sspboyd", canvasW - 76, canvasH - 11);
   };
 };
 
